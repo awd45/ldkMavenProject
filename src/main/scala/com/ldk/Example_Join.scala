9842d9f13a19d23d057471782a00d0717db7dab4
awd45 <awd45@naver.com>
2018-04-05 16:03:22 +0900

    start

---
+++ src/main/scala/com/ldk/Example_Join.scala
@@ -0,0 +1,35 @@
+package com.ldk
+
+import org.apache.spark.sql.SparkSession;
+
+object Example_Join {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("...").
+      config("spark.master", "local").
+      getOrCreate()
+
+    var mainFile = "kopo_channel_seasonality_ex.csv"
+    var subFile = "kopo_product_mst.csv"
+    var dataPath = "c:/spark/bin/data/"
+
+
+    // 상대경로 입력
+    var mainDataDf= spark.read.format("csv").
+      option("header", "true").load(dataPath + mainFile)
+    var subDataDf = spark.read.format("csv").
+      option("header", "true").load(dataPath + subFile)
+
+    mainDataDf.createOrReplaceTempView("mainTable")
+    subDataDf.createOrReplaceTempView("subTable")
+
+
+    //a.productgroup b.productname
+    spark.sql("select a.regionid,, a.productgroup, b.productname, a.yearweek, a.qty "+
+    "from mainTable a "+
+    "left join subTable b "+
+    "on a.productgroup = b.productid")
+
+
+
+  }
+}
