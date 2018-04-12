3ca4231b061ca4655bf48320fdde1ebe394af9e4
awd45 <awd45@naver.com>
2018-04-12 15:52:23 +0900

    start

--- src/main/scala/com/ldk/Example_Seasonality.scala
+++ src/main/scala/com/ldk/Example_Seasonality.scala
@@ -13,14 +13,16 @@ object Example_Seasonality {
     var staticUser = "kopo"
     var staticPw = "kopo"
     var selloutDb = "KOPO_CHANNEL_SEASONALITY_NEW"
-    var productNameDb = "KOPO_REGION_MST"
+    var productNameDb = "KOPO_PRODUCT_MASTER"
 
+    //가져온 데이터를 아래 dataframe으로 저장하고 가져온다.
     val selloutDf = spark.read.format("jdbc").
       options(Map("url" -> staticUrl, "dbtable" -> selloutDb, "user" -> staticUser, "password" -> staticPw)).load
 
     val productMasterDf = spark.read.format("jdbc").
       options(Map("url" -> staticUrl, "dbtable" -> productNameDb, "user" -> staticUser, "password" -> staticPw)).load
 
+    //가상의 테이블을 2개 만들어 담아준다.
     selloutDf.createOrReplaceTempView("selloutTable")
     productMasterDf.createOrReplaceTempView("mstTable")
   }
